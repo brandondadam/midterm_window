6ced21b3bb1afc0dd34d42c7a20c085be60f2b0e
bdadam <bdadam@mail.bradley.edu>
2015-10-18 04:59:11 -0500

    deleted some white space, and fixed typo in js

--- window.js
+++ window.js
@@ -7,17 +7,16 @@ $(document).ready(function(){
 		if(active.next().length > 0){
 			var next = active.next();
 		} else{
-			var next = $('image img:first');
+			var next = $('images img:first');
 		}
 		//get the next image ready by using z-index
 		next.css('z-index','2');
-
 		//fading out the class
 		active.fadeOut(1000,function(){
 			//move active image back, show it and remove the class
 			active.css('z-index', '1').show().removeClass('active');
 			//make next image the active one
-			next.css('z-index', '3').addClass('active');
+			next.css('z-index','3').addClass('active');
 		});
 	},3000);
 });
