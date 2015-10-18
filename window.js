534cd8517074dfed12a7c6e52d0b4ae5c286404d
bdadam <bdadam@mail.bradley.edu>
2015-10-18 03:43:42 -0500

    added a event to the photo

--- window.js
+++ window.js
@@ -1,2 +1,5 @@
 var div = document.getElementById('image');
-		div.innerHTML = '<img src="sample1.jpg">';
+div.innerHTML = '<img src="sample1.jpg">';
+div.addEventListener('click', function(){
+	console.log('next photo');
+}, false);
