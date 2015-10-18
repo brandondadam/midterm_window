0a3410456fddf88967feaca0dcdbdfa31faefa65
bdadam <bdadam@mail.bradley.edu>
2015-10-18 04:46:55 -0500

    added z-indexs to css, created, active class for images, and did all the js stuff

---
+++ window.js
@@ -0,0 +1,25 @@
+$(document).ready(function()){
+	setInterval(function(){
+		//get current active image
+		var active = $('images .active');
+
+		//if there is another images left then make that images next
+		//if not then go back to image 1
+		if(active.next().lenght > 0) {
+			var next = active.next();
+		} else {
+			var next = $('image img:first');
+		}
+
+		//get the next image ready by using z-index
+		next.css('z-index','2');
+
+		//fading out the class
+		active.fadeOut(1000,function(){
+			//move active image back, show it and remove the class
+			active.css('z-index', '1').show().removeClass('active');
+			//make next image the active one
+			next.css('z-index', '3').addClass('active');
+		});
+	},3000);
+});
